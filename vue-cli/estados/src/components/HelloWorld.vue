44c200ede094270e892ef06030dc0b392d41ff15
Darinel Cigarroa <darinelcigarroa97@gmail.com>
2021-08-29 00:17:41 -0500

    termine curso vuejs

--- vue-cli/estados/src/components/HelloWorld.vue
+++ vue-cli/estados/src/components/HelloWorld.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>{{ $store.state.msg }}</h1>
+  <div class="hello container">
+    <h1>{{ $store.getters.mensaje }}</h1>
+    <h2>{{ $store.getters.nombreCompleto }}</h2>
+    <h4>Amigos:</h4>
+    <ul>
+      <li v-for="(item, index) in $store.state.amigos" :key="index">
+        {{ item }}
+        <button v-if="$store.state.amigos" class="btn btn-danger btn-sm" @click="$store.dispatch('deleteFriendAction',index)">
+          Eliminar
+        </button>
+      </li>
+  </ul>
+    <input
+      type="text"
+      name="amigo"
+      id="amigo"
+      v-model="amigo"
+      @keyup.enter="addFriends"
+    />
+    <p>
+      <button class="btn btn-primary btn-sm" @click.prevent="addFriends">
+        Agregar amigo
+      </button>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      amigo: "",
+    };
+  },
+  methods: {
+    addFriends() {
+      this.$store.dispatch("addAmigoAction", this.amigo);
+      this.amigo = "";
+    }
+  },
+};
 </script>
 
 <style scoped>
@@ -18,11 +51,11 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
+}
+.btn {
+  margin: 4px 0px;
 }
 </style>
--- vue-cli/estados/src/store/index.js
+++ vue-cli/estados/src/store/index.js
@@ -2,12 +2,36 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    msg: 'Hola mundo'
+    msg: '¡Hola mundo! :D',
+    nombre: 'Darinel',
+    apellidos: 'Cigarroa De Los Santos',
+    amigos:[],
   },
   mutations: {
+    addAmigo(state, amigo){
+      state.amigos = [amigo,... state.amigos]
+    },
+    deleteFriend(state, index){
+      state.amigos.splice(index,1)
+    }
+   
   },
   actions: {
+    addAmigoAction(context,amigo){
+      context.commit('addAmigo',amigo)
+    },
+    deleteFriendAction(context,index){
+      context.commit('deleteFriend',index)
+    }
   },
   modules: {
+  },
+  getters:{
+    mensaje(state){
+      return state.msg
+    },
+    nombreCompleto(state){
+      return state.nombre + ' ' + state.apellidos
+    }
   }
 })
